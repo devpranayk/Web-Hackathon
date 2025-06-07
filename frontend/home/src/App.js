3e398e4585bc984ed592d657523ec742a421eb62
Pranay Kandikonda <pranayk74174174@gmail.com>
2025-06-08 00:41:28 +0530

    Modified App.js to include animated network background

--- frontend/home/src/App.js
+++ frontend/home/src/App.js
@@ -1,29 +1,29 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Authenication from './authenication/Authenication';
 import InterviewerLogin from './authenication/InterviewerLogin';
-import StudentSidebar from './students/Studentbar'
+import StudentSidebar from './students/Studentbar';
 import InterViewSideBar from './interviewer/InterviewSidebar';
 import FacultyLogin from './authenication/FacultyAuth';
-import FacultySidebar from './faculty/FacultySidebar'
+import FacultySidebar from './faculty/FacultySidebar';
 import Home from './Home';
 
 
 function App() {
- 
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path="/auth" element={<Authenication />} />
-        <Route path='/inter' element={<InterViewSideBar/>}/>
-        <Route path='/faculty-auth' element={<FacultyLogin/>}></Route>
+    <>
 
-        <Route path='/profile' element={<StudentSidebar/>}/>
-        <Route path='/faculty' element={<FacultySidebar/>}></Route>
-        <Route path='/in' element={<InterviewerLogin/>}></Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/auth" element={<Authenication />} />
+          <Route path='/inter' element={<InterViewSideBar />} />
+          <Route path='/faculty-auth' element={<FacultyLogin />} />
+          <Route path='/profile' element={<StudentSidebar />} />
+          <Route path='/faculty' element={<FacultySidebar />} />
+          <Route path='/in' element={<InterviewerLogin />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
